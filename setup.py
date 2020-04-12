1a78b625119dd391b5e42d24823681c2b78b8ec4
Dražen Žarić <drazen.zaric@gmail.com>
2020-04-12 02:45:37 +0200

    Updated package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="heatmapz",
-	version="0.0.3",
+	version="0.0.4",
 	author="Drazen Zaric",
 	author_email="drazen.zaric@gmail.com",
 	description="Create heatmaps with shapes and size as a parameter",
